a688beeba4c81b48f90b7cbd2e9a453a9ad19e79
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2013-10-17 15:01:02 -0400

    uniqueness on users.email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
   def new
-    
+    render :new
+  end
+
+  def create
+    whitelisted = params.require(:user).permit(:email)
+    user = User.new(whitelisted)
+
+    if user.save
+      redirect_to user_path(user)
+    else
+      redirect_to new_user_path
+    end
   end
 
 end
