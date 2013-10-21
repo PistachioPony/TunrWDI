5477c3d5363c3b355583bf42887a53763a353639
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2013-10-21 11:51:30 -0400

    balance

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131021153251) do
+ActiveRecord::Schema.define(version: 20131021154943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,10 +39,11 @@ ActiveRecord::Schema.define(version: 20131021153251) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",           null: false
+    t.string   "email",                         null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest", null: false
+    t.string   "password_digest",               null: false
+    t.decimal  "balance",         default: 0.0, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
